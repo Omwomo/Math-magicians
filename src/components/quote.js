ba2b441063c1234379a6d6c5f6e2276c4e2d98ca
Omwomo <eugeneoduor79@gmail.com>
2023-08-26 16:45:55 +0300

    Initiated DisplayQuotes function

---
+++ src/components/quote.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+
+export default function DisplayQuotes() {
+  const [newQuote, setNewQuote] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const category = 'learning';
+  const apiKey = 'MhhhjXkGrj3RilcESlI0hg==6uiWhYZSR6osRzHw';
+  const apiUrl = `https://api.api-ninjas.com/v1/quotes?category=${category}`;
+
+  const fetchQuotes = () => {
+    fetch(apiUrl, {
+      method: 'GET',
+      headers: { 'X-Api-Key': apiKey },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setNewQuote(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+      });
+  };
+
+  useEffect(() => {
+    fetchQuotes();
+  }, [apiUrl]);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return (
+      <p>
+        Error:
+        {' '}
+        {error.message}
+      </p>
+    );
+  }
+
+  return (
+    <ul className="quotes">
+      {newQuote.map((item) => (
+        <>
+          <li className="quote" key={item.id}>
+            {item.quote}
+            {' '}
+            {item.author}
+          </li>
+          <button key={`button-${item.id}`} onClick={fetchQuotes} type="button" className="qoute-button">Quote Button</button>
+
+        </>
+      ))}
+    </ul>
+  );
+}
