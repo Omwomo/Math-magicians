084eddc9ff4924fed4f2ec48def01f250cd7b9a1
Omwomo <eugeneoduor79@gmail.com>
2023-08-30 21:43:33 +0300

    Made some adjustments to the existing componets to achieve project requirements

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default function Navigation() {
+function Navigation() {
   return (
     <nav className="navbar">
       <div className="logo">
         Math Magicians
       </div>
       <ul className="nav-links">
-        <li>Home</li>
+        <li><Link to="/home">Home</Link></li>
         <li><Link to="/calculator">Calculator</Link></li>
-        <li><Link to="/DisplayQuotes">Quotes</Link></li>
+        <li><Link to="/quote">Quotes</Link></li>
       </ul>
     </nav>
   );
 }
+
+export default Navigation;
