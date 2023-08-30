79638e1e3fd5c7db0e36f7d530ee3c30401cf8f2
Omwomo <eugeneoduor79@gmail.com>
2023-08-30 21:40:21 +0300

    Created homepage component

---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function HomePage() {
+  return (
+    <div className="home-page">
+      <h2>What I learnt while building the project</h2>
+      <ul className="intro">
+        <li>Set up a React app.</li>
+        <li>Use React documentation.</li>
+        <li>Use React components.</li>
+        <li>Handle events in a React app.</li>
+        <li>Use React life cycle methods.</li>
+        <li>Understand the mechanism of lifting state up.</li>
+        <li>Use React state.</li>
+        <li>Use the correct React hook when fetching data</li>
+        <li>Connect to an API within a functional component</li>
+        <li>Show different UI outputs depending on the component state</li>
+        <li>Use React Router.</li>
+        <li>Use styles in a React app.</li>
+      </ul>
+    </div>
+  );
+}
